a1a19d32aa00e0b98da2b446730c5787cd232be9
Arnoldo Bezerra Pinheiro <arnoldobepi@gmail.com>
2023-09-22 10:57:28 -0300

    Variable Type

---
+++ src/VariableType.java
@@ -0,0 +1,10 @@
+public class VariableType {
+    public static void main(String[] args) throws Exception {
+        int yearsOld; // "int" type indicate that this variable is an integer number
+        double height = 3.8; //"double" type indicate that this variable can have a decimal number
+
+        short shortNumber = 1;
+        int normalNumber = shortNumber;
+        short shorNumberTwo = (short) normalNumber;
+    }
+}
